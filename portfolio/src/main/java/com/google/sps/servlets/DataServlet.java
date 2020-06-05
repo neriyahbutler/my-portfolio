5308a42ccfa49ab0e690fd19d085f1032165f2ea
neriyahbutler <neriyah.butler@gmail.com>
2020-06-05 07:20:59 +0000

    adds features that stores and displays comments, changes code to align with google's code guidelines

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,6 +14,8 @@
 
 package com.google.sps.servlets;
 
+import java.util.*;
+import java.io.*;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -24,9 +26,39 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
+  ArrayList<String> commentArray = new ArrayList<String>();
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
+    String json = convertToJson(commentArray);
+
+      response.setContentType("application/json");
+      response.getWriter().println(json);
+  }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String comment = getComments(request);
+    commentArray.add(comment);
+
+    response.sendRedirect("/index.html");
+  }
+
+  public String getComments(HttpServletRequest request) {
+    String value = request.getParameter("comment-input");
+
+    if (value == null) { return "";}
+    return value;
+  }
+
+  public String convertToJson(ArrayList<String> comments){
+    String json = "{";
+    for (int i = 0; i < comments.size(); i++){
+      if (i != 0){ json += ", ";}
+        json += "\"" + Integer.toString(i) +"\": ";
+        json += "\"" + comments.get(i) + "\"";
+      }
+    json += "}";
+    return json;
   }
 }
--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,17 +12,17 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * Adds a random greeting to the page.
- */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+function getComments(){
+  fetch('/data').then(response => response.json()).then((text) => {
+    const commentContainer = document.getElementById('commentList');
+    const jsonValues = Object.values(text);
 
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+    console.log(jsonValues);
+    
+    for(var i = 0; i < Object.values(text).length; i++){
+      var comment = document.createElement('p');
+      comment.innerHTML = jsonValues[i];
+      commentContainer.appendChild(comment);
+    }
+  })
 }
