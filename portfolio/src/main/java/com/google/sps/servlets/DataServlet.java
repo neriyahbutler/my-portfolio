c4e60095b8cf49cf86554a5e421ea748bae38522
neriyahbutler <neriyah.butler@gmail.com>
2020-06-05 07:30:23 +0000

    adds commenting feature, changes code to match google's html/css/js/java guide

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -11,22 +11,54 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+ 
 package com.google.sps.servlets;
-
+ 
+import java.util.*;
+import java.io.*;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+ 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-
+ 
+  ArrayList<String> commentArray = new ArrayList<String>();
+ 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
+    String json = convertToJson(commentArray);
+ 
+      response.setContentType("application/json");
+      response.getWriter().println(json);
+  }
+ 
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String comment = getComments(request);
+    commentArray.add(comment);
+ 
+    response.sendRedirect("/index.html");
+  }
+ 
+  public String getComments(HttpServletRequest request) {
+    String value = request.getParameter("comment-input");
+ 
+    if (value == null) { return "";}
+    return value;
+  }
+ 
+  public String convertToJson(ArrayList<String> comments){
+    String json = "{";
+    for (int i = 0; i < comments.size(); i++){
+      if (i != 0){ json += ", ";}
+        json += "\"" + Integer.toString(i) +"\": ";
+        json += "\"" + comments.get(i) + "\"";
+      }
+    json += "}";
+    return json;
   }
 }
